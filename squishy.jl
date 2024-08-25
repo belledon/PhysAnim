3d36b8ced5c8e9d3d7d0fdf7e005043c8019e6af
belledon <mbelledonne@gmail.com>
2024-08-25 12:44:55 -0400

    reset `Project.toml` and added occluder

--- squishy.jl
+++ squishy.jl
@@ -1,14 +1,10 @@
 using Javis, Animations, Colors
+using Luxor: setmode
 
 function ground(args...)
     background("black")
     sethue("white")
 end
-
-function inner_evolve()
-end
-
-
 
 inner_anim(radius) = Animation(
     [0, .15, .55, 0.85, 1],
@@ -17,6 +13,7 @@ inner_anim(radius) = Animation(
     [linear(), expin(3.0), linear(), linear()]
 )
 
+Y = 600
 T = 100
 col_frac = 0.5
 col_dur = 0.03
@@ -31,31 +28,51 @@ diff_radii = outer_radius - inner_radius
 outer_anim(col_frac, col_dur) = Animation(
     [0, col_frac, col_frac + col_dur, col_frac + 2 * col_dur, 1],
     [
-        Point(0, -50),
-        Point(0, 200-outer_radius),
-        Point(0, 200-inner_radius),
-        Point(0, 200-outer_radius),
-        Point(0, -50)
+        Point(0, -div(Y, 2)+3*outer_radius),
+        Point(0, (div(Y, 2)-outer_radius)),
+        Point(0, (div(Y, 2)-inner_radius)),
+        Point(0, (div(Y, 2)-outer_radius)),
+        Point(0, -div(Y, 2)+3*outer_radius)
     ],
     [polyin(2), linear(), linear(), polyout(2)]
 )
 
-video = Video(400, 400)
+# color_anim = Animation(
+#     [0.0,1.0], # must go from 0 to 1
+#     [
+#         Lab(RGB(1.0, 0.0, 0.0)),
+#         Lab(RGB(0.0, 0.0, 1.0))],
+#     linear(),
+# )
+
+color_anim = Animation(
+    [0, 0.5, 1], # must go from 0 to 1
+    [
+        Lab(colorant"red"),
+        Lab(colorant"cyan"),
+        Lab(colorant"black"),
+    ],
+    [sineio(), sineio()],
+)
+
+video = Video(400, Y)
 Background(1:T, ground)
 
 # Floor
 Object(1:T,
     JLine(
-        Point(-100, 200), Point(100, 200),
-        color = "darkgray",
+        Point(-100, div(Y, 2)), Point(100, div(Y, 2)),
+        color = "white",
         linewidth = 5,
     ),
 )
 
+
+# Ball
 l1 = @JLayer 1:T begin
     outer = Object(1:T, JEllipse(O, 2 * outer_radius, 2 * outer_radius;
-                                 color = "white", action = :stroke,
-                                 linewidth = 5.0))
+                                 color = Lab(RGB(1.0, 0.0, 0.0)),
+                                 action = :fill))
     # inner = Object(1:T, JCircle(inner_radius; color = "white", action = :fill), O)
     act!(outer, Action((col - 1):(col + comp_steps),
                        change(:h, 2 * outer_radius => 2 * inner_radius)))
@@ -65,14 +82,24 @@ l1 = @JLayer 1:T begin
                        change(:w, 2 * outer_radius => 3.0 * outer_radius)))
     act!(outer, Action((col + comp_steps):(col + 2 * comp_steps),
                        change(:w,  3.0 * outer_radius => 2 * outer_radius)))
-end
+    act!(outer, Action(30:T, change(:color, "blue")))
 
+
+end
 act!(l1, Action(1:T, outer_anim(col_frac, col_dur), translate()))
+
+# Occluder
+OCC_W = 4 * outer_radius
+OCC_H = 6 * outer_radius
+OCC_P = O - Point(div(OCC_W, 2), div(OCC_H, 2) + 10)
+l2 = @JLayer 1:T begin
+    Object(1:T, JRect(OCC_P, OCC_W, OCC_H,  color = "white", action=:fill))
+end
 
 isdir("output") || mkdir("output")
 
 render(
     video;
-    # liveview=true,
-    pathname = "output/squishy.mp4",
+    liveview=true,
+    # pathname = "output/squishy.mp4",
 )
